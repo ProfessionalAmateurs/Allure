8f5dd2ac2df1ae1a3e4cd1d9c131d5364ce47e8a
saniyaarab <capstonefinal2018@gmail.com>
2018-11-14 10:57:53 -0500

    Comit to the local branch

---
+++ Source/Workspaces/AllureRemodeling/AllureRemodeling/AllureRemodeling/Models/Materials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllureRemodeling.Models
+{
+    public class Materials
+    {
+        public virtual int MaterialID { get; set; }
+        public virtual string Description { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
