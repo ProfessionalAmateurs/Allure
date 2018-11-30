8162b850aa3e7a740980ca090d01e50728f61fba
Saniya <capstonefinal2018@gmail.com>
2018-11-29 20:30:14 -0500

    Sania'Changes - Adding Estimates And Database class changes.

---
+++ AllureRemodeling/Models/Estimates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllureRemodeling.Models
+{
+    public class Estimates
+    {
+        public int QuestionID
+        { get; set; }
+
+        public string Question
+        { get; set; }
+
+        public int UserID
+        { get; set; }
+
+        public string Answer
+        { get; set; }
+    }
+}
